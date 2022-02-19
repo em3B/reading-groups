ba4ac5c8d6bb4071990455761369e230829c5f02
Emily Baird <emilym.baird@gmail.com>
2022-02-19 14:58:50 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # student routes
+  resources :students do
+    resources :reading_groups, only: %i[index show] do
+      resources :daily_readings, through: :texts, only: %i[show] do
+        resources :questions, only: %i[show] do
+          resources :answers, only: %i[show new create update]
+        end
+      end
+    end
+  end
+
+  # teacher routes
 end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
