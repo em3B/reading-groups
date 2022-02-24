df4621e9d33d6a87978fc708a4f605e09501f25e
Josh Kennedy <joshkennedybjj@gmail.com>
2022-02-24 21:15:49 +0000

    buttons fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# Text.destroy_all
+
+# harry_potter_philosophers_stone = Text.new({
+#   title: "Harry Potter and the Philosopher's Stone",
+#   author: 'J. K. Rowling',
+# })
+
+# harry_potter_philosophers_stone.words.attach(io: File.open(Rails.root.join('app','assets','texts','harry_potter_philosophers_stone','harry_potter_philosophers_stone.txt')), filename: 'harry_potter_philosophers_stone.txt', content_type: 'text/plain')
+# harry_potter_philosophers_stone.photo.attach(io: File.open(Rails.root.join('app','assets','texts','harry_potter_philosophers_stone','harry_potter_philosophers_stone.jpg')), filename: 'harry_potter_philosophers_stone.jpg', content_type: 'image/jpg')
+# harry_potter_philosophers_stone.save
+
+# # jean_duck_picard = Duck.new({
+# #   name: 'Jean-Duck Picard',
+# #   description: 'Totally unflappable, this Duck Buddy is a firm favourite',
+# #   age: 53,
+# #   nationality: 'French'
+# # })
+# # jean_duck_picard.photo.attach(io: File.open(Rails.root.join('app','assets','images','jean-duck-picard.jpg')), filename: 'jean-duck-picard.jpg', content_type: 'image/jpg')
+# # jean_duck_picard.save
+
+# # @message.image.attach(io: File.open('/path/to/file'), filename: 'file.pdf', content_type: Rails.root.join('app','icatio','/pdf')','
+# # require "open-uri"
+
+# # file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg')
+# # article = Article.new(title: 'NES', body: "A great console")
+# # article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+
+# # 10.times do
+# #   Duck.create(name: Faker::GreekPhilosophers.name, photo: ducks_urls.sample, description: Faker::GreekPhilosophers.quote,
+# #               nationality: Faker::Nation.nationality, age: Faker::Number.between(from: 18, to: 50))
+# # end
