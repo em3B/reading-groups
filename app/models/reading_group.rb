84e1747b61f74303030cb18df58d59c04687117c
James Britton <brittonjp@gmail.com>
2022-02-24 20:37:40 +0000

    updated activerecord associations and schema

--- app/models/reading_group.rb
+++ app/models/reading_group.rb
@@ -1,3 +1,8 @@
 class ReadingGroup < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
+  has_many :members
+  has_many :users, through: :members
+  has_many :daily_readings
+  has_many :questions, through: :daily_readings
+  has_many :texts, through: :daily_readings
 end
