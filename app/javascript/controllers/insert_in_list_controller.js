c225dab0bbf5dd977f30830e112bc740da7760ef
Emily Baird <emilym.baird@gmail.com>
2022-03-03 17:16:37 +0000

    change font spacing

---
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "stimulus";
+import { csrfToken } from "@rails/ujs";
+
+export default class extends Controller {
+  static targets = ['items', 'form'];
+
+  connect() {
+    console.log(this.element);
+    console.log(this.itemsTarget);
+    console.log(this.formTarget);
+  }
+  send(event) {
+  event.preventDefault();
+
+  console.log('TODO: send request in AJAX');
+
+  event.preventDefault();
+
+  fetch(this.formTarget.action, {
+    method: 'POST',
+    headers: { 'Accept': "application/json", 'X-CSRF-Token': csrfToken() },
+    body: new FormData(this.formTarget)
+  })
+    .then(response => response.json())
+    .then((data) => {
+      if (data.inserted_item) {
+        this.itemsTarget.insertAdjacentHTML("beforeend", data.inserted_item);
+      }
+        this.formTarget.outerHTML = data.form;
+    });
+}
+}
+// [...]
