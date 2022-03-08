eafa4cfca7f0fe8916a41cfa918c091d0bf7fd06
Emily Baird <emilym.baird@gmail.com>
2022-03-08 20:13:13 +0000

    extension question area and objective add

---
+++ app/javascript/components/objective.js
@@ -0,0 +1,14 @@
+const bar = document.querySelector(".objective-bar");
+const title = document.querySelector(".objective-title");
+const content = document.querySelector(".objective-content");
+
+function displayObjective() {
+  if (document.body.contains(content)) {
+    title.addEventListener("click", function() {
+      console.log("hi");
+      jQuery(content).fadeOut(3000);
+    })
+  }
+}
+
+export { displayObjective };
---
+++ app/javascript/components/unlight.js
@@ -0,0 +1,18 @@
+
+const text = document.querySelector(".daily-text");
+
+
+function unlight() {
+  if (text.classList.contains(".highlighted-text")) {
+    text.addEventListener("mouseup", function() {
+      let selection = document.getSelection();
+      let range = selection.getRangeAt(0);
+      let newNode = document.createElement("span");
+      newNode.classList.remove("highlighted-text");
+      newNode.classList.add("no-highlight");
+      range.surroundContents(newNode);
+    })
+  }
+}
+
+export {unlight};
