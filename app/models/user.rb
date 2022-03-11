30f66bc341e8b6a449cdcd3e72efae540be826e1
James Britton <brittonjp@gmail.com>
2022-03-11 20:57:12 +0000

    added list of students to teacher's daily_reading show view and added devise functionality to see if they've logged in within the last hour

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
   # has_many :users, through: :reading_groups
   has_many :members
   has_many :answers
@@ -10,4 +10,10 @@ class User < ApplicationRecord
   has_many :questions, through: :answers
   has_many :messages
   has_many :chatrooms, through: :messages
+
+  def online?
+    if self.last_sign_in_at
+      last_sign_in_at > 1.hour.ago
+    end
+  end
 end
